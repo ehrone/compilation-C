a84780557cd0c83d4c7c11ff7eb958806ff7fbc9
ehrone <83878253+ehrone@users.noreply.github.com>
2022-10-14 22:04:56 +0200

    Update etudiant2.c

--- TP2/etudiant2.c
+++ TP2/etudiant2.c
@@ -38,8 +38,6 @@ void creat_eleves(data *p, int nb)
         printf("Note en prog C %i :\n ", (i+1));
         scanf("%f", &((p+i)->prog_C)); 
 
-      //  printf("%f\n", *((p+i)->prog_C));
-
         c = getchar();
 
         printf("Note en Système d'exploitation %i :\n ", (i+1));
@@ -80,7 +78,15 @@ void creat_eleves(data *p, int nb)
 int main (void)
 {
 
-    data eleves[5]; // on crée un tableau de structures de type data
+    data eleves[5] =
+    {
+        {"nom 1", "prenom 1", "addresse 1", "note 1.1", "note 1.1"},
+        {"nom 2", "prenom 2", "addresse 2", "note 2.1", "note 2.1"},
+        {"nom 3", "prenom 3", "addresse 3", "note 3.1", "note 3.1"},
+        {"nom 4", "prenom 4", "addresse 4", "note 4.1", "note 4.1"},
+        {"nom 5", "prenom 5", "addresse 5", "note 5.1", "note 5.1"}
+
+    };; // on crée un tableau de structures de type data
 
     // on crée nos eleves
     creat_eleves(eleves, nb_eleves);
