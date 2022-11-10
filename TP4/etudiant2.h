55fa60e6ea39fab2315f7abd20aba08b0f93a4a8
Emma Begard <emma.begard@cpe.fr>
2022-11-10 17:38:43 +0100

    save avant de partir le jeudi 10

---
+++ TP4/etudiant2.h
@@ -0,0 +1,4 @@
+void creat_eleves(data *p, int nb);
+void disp_eleves(data *p, int nb);
+void save_eleve(data *tableau, char *chemin, int nb);
+void save_eleve_2(data *tableau, char *chemin, int nb_eleves);
--- TP4/fichier.c
+++ TP4/fichier.c
@@ -5,8 +5,9 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#define TAILLEMAX 1000
 
-void lire_fichier(char *chemin)
+char lire_fichier(char *chemin)
 { 
     char data[1000];
 
@@ -26,4 +27,11 @@ void ecrire_fichier(char *chemin, char *to_add, int len_to_add)
     int file = open(chemin, O_CREAT|O_WRONLY, S_IRUSR|S_IWUSR);
     int size = write(file, to_add, len_to_add);
     close(file);
+}
+
+chercher(char * tableau)
+
+void chercherfichier()
+{
+
 }
