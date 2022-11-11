a1c1985365b05edf7a1c995a4d51a45baa1b1c93
ehrone <83878253+ehrone@users.noreply.github.com>
2022-11-11 22:34:36 +0100

    Update repertoire.c

--- TP5/src/repertoire.c
+++ TP5/src/repertoire.c
@@ -25,7 +25,10 @@ int main(int argc, char **argv)
 }
 
 /*------------------------------------------------------------------*/
-
+/* Cette fonction lit le contenu d'un dossier et affiche le nom des fichiers 
+ * entrée:
+ *      char *chemin : chemin menant au dossier
+*/
 void lire_dossier(char *chemin)
 {
    
@@ -45,28 +48,28 @@ void lire_dossier(char *chemin)
 
         if(ent==NULL){break;} // si on a déjà lu tt les noms de fichiers dans le dossier on sort du while
         
-        printf(" %s \n", ent->d_name); // on affiche le nom des fichiers du dossier
+        printf(" %s \n", ent->d_name); // on affiche le nom du fichier
     }
 
     closedir(dir);// on ferme le dossier 
 
 }
 
-
+/* Cette fonction lit le contenu d'un dossier et affiche le nom des fichiers en récursif
+ * entrée:
+ *      char *chemin : chemin menant au dossier
+*/
 void lire_dossier_recursif(char *chemin, int counter)
 {
-    //printf("nivau %d \n", counter);
-
+  
     DIR *dirp = opendir(chemin); // on ouvre le dossier dont le chemin est spécifié en 1er argument dans le terminal.
    
-    
     if (dirp==NULL)
     {
         printf("Erreur ouverture du fichier\n ");
         return;
     
     }
-
 
     struct dirent * ent;// la structure qui permet de récupérer l'entrée des fichiers dans le dossier ouvert
 
@@ -79,26 +82,26 @@ void lire_dossier_recursif(char *chemin, int counter)
             //printf("plus rien dans le dossier ! \n " );
             break;
         } 
-        
-        for (int i=0; i< counter; i++){ printf("   ");}
-       
+  
 
-        if(ent->d_type==DT_DIR)// si il s'agit d'un dossier
+        if(ent->d_type==DT_DIR)// si il s'agit d'un sous-dossier on lit ce qu'il y a à l'interrieur
         {
             //printf("%s c'est un sous dossier\n", ent->d_name);
             if ( (strcmp(ent->d_name,".") !=0) && (strcmp(ent->d_name,"..") !=0) )
             {   
                 printf("Dossier : %s \n ", ent->d_name); // on affiche le nom des fichiers du dossier
+                
+                // composition de la route pour lire le sous-dossier
                 char route[256]="";
                 strcat(route, chemin);
                 strcat(route, "/");
                 strcat(route , ent->d_name);
                 //printf(" Route : %s \n",route);
-                lire_dossier_recursif(route, 1+counter);
+                lire_dossier_recursif(route, 1+counter); // récursivité
             }
                
         }
-        else 
+        else // c'est un simple fichier on affiche son nom
         {
             printf("%s \n", ent->d_name);
         }
@@ -109,7 +112,6 @@ void lire_dossier_recursif(char *chemin, int counter)
     closedir(dirp);// on ferme le dossier 
 
 }
-
 
 void lire_dossier_iteratif(char *chemin, int counter)
 {
